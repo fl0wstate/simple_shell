601cf4b593e195320d82830e3f55571b6a0623be
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-08 07:47:47 +0300

    Clean up _realloc

--- _realloc.c
+++ _realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (!ptr)
-		return (malloc(new_size * sizeof(ptr)));
+		return (malloc(new_size));
 
 	return (adjust_book(ptr, old_size, new_size));
 }

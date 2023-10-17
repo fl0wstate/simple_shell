3faa69f2d6eb6dae1663c7c2c1b2948fc75196e0
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-17 18:57:02 +0300

    Comment non_interactive, n bettify

--- non_interactive.c
+++ non_interactive.c
@@ -7,38 +7,11 @@
 void non_interactive(m_args *mode_args)
 {
 	char *path = *mode_args->path;
-	pid_t fk_id;
-	/*TODO: non-interactive sucks actually */
 
+	/*TODO: non-interactive sucks actually */
 	if (!path)
 		printf("%s: %u: %s: not found\n",
 				*mode_args->av, *mode_args->cmd_count, **mode_args->tokens);
 	else
-	{
-		fk_id = fork();
-		if (fk_id == -1)
-		{
-			/* fork error */
-			perror("fork");
-		}
-		if (!fk_id)
-		{
-			execve(path, *mode_args->tokens, *mode_args->env);
-		}
-		else
-		{
-			/* parent */
-			wait(0);
-			free_safe(mode_args);
-			free_list(*mode_args->list_path);
-			/*exit(EXIT_SUCCESS);*/
-		}
-	}
-
-#if 0
-	free_buf(0, mode_args->path, 0);
-	free_buf(0, mode_args->line, 0);
-	free_buf(mode_args->tokens, 0, 1);
-	exit(0);
-#endif
+		execve(path, *mode_args->tokens, *mode_args->env);
 }

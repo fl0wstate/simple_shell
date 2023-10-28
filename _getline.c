a82a774de024366068489b467a3620957567f638
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-28 20:32:36 +0300

    Fix mutli-line commands

--- _getline.c
+++ _getline.c
@@ -14,7 +14,6 @@ ssize_t _getline(char **line, size_t *len, FILE *stream)
 	ssize_t bytes = 0;
 	int count = 0;
 
-	/*TODO: handle errors with allocation */
 	*len = !*line || !*len ? init : *len;
 	if (!*line || *len == 0)
 	{
@@ -22,12 +21,11 @@ ssize_t _getline(char **line, size_t *len, FILE *stream)
 		if (!*line)
 			return (getline_error("malloc"));
 	}
-	while ((bytes = read(stream->_fileno, *line + i, BUFFER)) > REOF)
+	while ((bytes = read(stream->_fileno, (&(*line)[i++]), 1)) > REOF)
 	{
-		count++;
-		i += bytes;
 		if (i >= init)
 		{
+			count++;
 			old = init;
 			init *= 2;
 			*len = init;
@@ -38,22 +36,20 @@ ssize_t _getline(char **line, size_t *len, FILE *stream)
 		if ((*line)[i - 1] == '\n')
 			break;
 	}
-	if (bytes == REOF)
-	{
-		(*line)[i] = 0;
-		return (EOF);
-	}
-	if (count > 2)
-		*len = i + 1;
 	(*line)[i] = 0;
 
+	if (count >= 2)
+		*len = i + 1;
+	if (bytes == REOF)
+		return (EOF);
 	return (i);
 }
+
 /**
  * getline_error - print error and exit
  * @str: string of error
  *
- * Return: -1 as error number
+ * Return: EOF
  */
 int getline_error(char *str)
 {
